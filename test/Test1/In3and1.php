2a1226ff27be6efea233ac4c45248b838bac1d81
mddejong <mjong@magnafacta.nl>
2018-08-17 17:01:56 +0200

    Added test files for standard class loading (without services)

---
+++ test/Test1/In3and1.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *
+ * @package    Test1
+ * @subpackage In3and2and1
+ * @author     Matijs de Jong <mjong@magnafacta.nl>
+ * @copyright  Expression copyright is undefined on line 42, column 18 in Templates/Scripting/PHPClass.php.
+ * @license    No free license, do not copy
+ */
+
+namespace Test1;
+
+/**
+ *
+ * @package    Test1
+ * @subpackage In3and2and1
+ * @copyright  Expression copyright is undefined on line 54, column 18 in Templates/Scripting/PHPClass.php.
+ * @license    No free license, do not copy
+ * @since      Class available since version 1.8.4 17-Aug-2018 16:41:20
+ */
+class In3and1
+{
+
+}
--- test/Zalt/Loader/BasicLoaderTest.php
+++ test/Zalt/Loader/BasicLoaderTest.php
@@ -23,13 +23,91 @@ use Zalt\Loader\ProjectOverloader;
  */
 class BasicLoaderTest extends \PHPUnit_Framework_TestCase
 {
-    public function testFirst()
+    /**
+     *
+     * @var ProjectOverloader
+     */
+    protected $overLoader;
+
+    /**
+     *
+     * @return ProjectOverloader
+     */
+    public function getBasicProjectOverloader()
     {
-        $loader = new ProjectOverloader([
-            'Test1',
-            'Test2',
+        return $this->overLoader;
+    }
+
+    /**
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        $this->overLoader = new ProjectOverloader([
             'Test3',
+            'Test2',
+            'Test1',
             ]);
+    }
+
+    public function providerLoadClass()
+    {
+        return [
+            ['Test1', 'OnlyIn1'],
+            ['Test2', 'OnlyIn2'],
+            ['Test3', 'OnlyIn3'],
+            ['Test3', 'In3and2and1'],
+        ];
+    }
+
+    public function providerLoadInstanceOf()
+    {
+        return [
+            ['In3and2and1', 'Test1\\In3and2and1'],
+            ['In3and2and1', 'Test2\\In3and2and1'],
+            ['In3and2and1', 'Test3\\In3and2and1'],
+            ['In3and2', 'Test3\\In3and2'],
+            ['In3and2', 'Test2\\In3and2'],
+            ['In3and1', 'Test3\\In3and1'],
+            ['In3and1', 'Test1\\In3and1'],
+            ['In2and1', 'Test2\\In2and1'],
+            ['In2and1', 'Test1\\In2and1'],
+        ];
+    }
+
+    public function testClassLoader()
+    {
+        $loader = $this->getBasicProjectOverloader();
         $this->assertEquals(get_class($loader), 'Zalt\Loader\ProjectOverloader');
+    }
+
+    /**
+     *
+     * @param string $namespace
+     * @param string $subClass
+     *
+     * @dataProvider providerLoadClass
+     */
+    public function testLoadClass($namespace, $subClass)
+    {
+        $loader = $this->getBasicProjectOverloader();
+        $class  = $loader->find($subClass);
+        $this->assertEquals($class, $namespace . '\\' . $subClass);
+    }
+
+    /**
+     *
+     * @param string $namespace
+     * @param string $subClass
+     *
+     * @dataProvider providerLoadInstanceOf
+     */
+    public function testLoadInstanceOf($subClass, ...$instances)
+    {
+        $loader = $this->getBasicProjectOverloader();
+        $class =  $loader->create($subClass);
+        foreach ($instances as $instance) {
+            $this->assertEquals(true, $class instanceof $instance);
+        }
     }
 }
