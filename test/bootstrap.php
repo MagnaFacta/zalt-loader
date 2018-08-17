1f88fa6ac8fbcd1b3747175e6f77cfce7ec0365e
mddejong <mjong@magnafacta.nl>
2018-08-17 17:27:48 +0200

    More settings for test

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -12,6 +12,8 @@ date_default_timezone_set('Europe/Amsterdam');
 // Set up autoload.
 if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
     require_once __DIR__ . '/../vendor/autoload.php';
+} elseif (file_exists(__DIR__ . 'vendor/autoload.php')) {
+    require_once __DIR__ . 'vendor/autoload.php';
 } elseif (file_exists(__DIR__ . '/../../../autoload.php')) {
     require_once __DIR__ . '/../../../autoload.php';
 }
